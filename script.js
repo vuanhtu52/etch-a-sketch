7bb9103881fd5e0894539c4c06bd2d432bd51c68
Tu Vu <vuanhtu52@gmail.com>
2023-07-14 15:24:43 +0700

    Let user change the grid size with the slider

--- script.js
+++ script.js
@@ -29,11 +29,29 @@ function toggleGridLines(showGridLines) {
     }
 }
 
+// Display the squares at the beginning
 createSquares(16);
 toggleGridLines(true);
 
-// Toggle the grid lines when user click the switch
+// Toggle the grid lines when user clicks the switch
 const toggleSwitch = document.querySelector("#toggle-switch");
 toggleSwitch.addEventListener("change", event => {
     toggleGridLines(event.currentTarget.checked);
+});
+
+// Change the number of squares when user changes the slider's value
+const grid = document.querySelector("#grid");
+const slider = document.querySelector("#size-slider");
+slider.addEventListener("change", event => {
+    while (grid.lastElementChild) {
+        grid.removeChild(grid.lastElementChild);
+    }
+    createSquares(event.currentTarget.value);
+    toggleGridLines(toggleSwitch.checked);
+});
+
+// Update the grid size's display while user is dragging the slider
+const sliderDisplay = document.querySelector(".slider-display");
+slider.addEventListener("input", event => {
+    sliderDisplay.textContent = `${event.currentTarget.value} x ${event.currentTarget.value}`;
 })
